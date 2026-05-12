d2bd085190a1f74571a6427c172065be6083bcbd
lorenzo.masserano <lorenzo.masserano@spindox.it>
2026-05-12 17:01:13 +0200

    66. Plus One

--- exercise/easy/PlusOne.kt
+++ exercise/easy/PlusOne.kt
@@ -8,29 +8,17 @@ fun main(){
 
 fun plusOne(digits: IntArray): IntArray {
 
-    if(digits[digits.size -1] != 9) {
-            digits[digits.size -1]++
-            return digits
-    } else {
-        if(digits.size > 1 && digits[digits.size -2] == 9){
-            println("ciao")
-            digits[digits.size -1] = 1
-            val digitsPlus = digits + intArrayOf(0)
-            for(index in digitsPlus.size -3 downTo 0){
-                if(digitsPlus[index] == 9){
-                        digitsPlus[index] = 1   
-                }
-            }
-            return digitsPlus
-        } else if(digits.size > 1) {
-            digits[digits.size -1] = 0
-            digits[digits.size -2]++
+    for(index in digits.size -1 downTo 0){
+        if(digits[index] == 9 && index != 0) {
+            digits[index] = 0
+        } else if (digits[index] != 9) {
+            digits[index]++
             return digits
         } else {
-            val digitsPlus = digits + intArrayOf(0)
-            digitsPlus[digitsPlus.size -2] = 1
-            return digitsPlus
+            digits[index] = 1
+            return digits + intArrayOf(0)
         }
     }
+
     return digits
 }
