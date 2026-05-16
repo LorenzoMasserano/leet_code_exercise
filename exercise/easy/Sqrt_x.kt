52bfe140fdac2c6c977fe79ec6c1a95e3c9d98c5
lorenzo.masserano <lorenzo.masserano@spindox.it>
2026-05-16 22:10:05 +0200

    Add 69.Sqrt

--- exercise/easy/Sqrt_x.kt
+++ exercise/easy/Sqrt_x.kt
@@ -1,6 +1,18 @@
-//kotlinc Sqrt_x.kt -include-runtime -d Sqrt_x.jar && java -jar Sqrt_x.jar
+// kotlinc Sqrt_x.kt -include-runtime -d Sqrt_x.jar && java -jar Sqrt_x.jar
 
 fun main() {
-  val value = value 
+
+  println(mySqrt(8))
 }
 
+fun mySqrt(x: Int): Int {
+
+  if(x == 0) return 0
+  var r = x.toLong()
+
+  while (r * r > x) {
+    r = (r + x / r) / 2
+  }
+
+  return r.toInt()
+}
